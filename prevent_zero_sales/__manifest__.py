2bc779c8af80ae640c21003ff14fefac245b16ea
Juan Carlos Arias Botero <42156547+juanca2918@users.noreply.github.com>
2026-01-26 21:32:02 -0500

    feat: introduce module to prevent confirming zero-amount invoices and Sales orders.

--- prevent_zero_sales/__manifest__.py
+++ prevent_zero_sales/__manifest__.py
@@ -16,6 +16,8 @@
     'category': 'Accounting',
     'version': '18.0.1.0.0',
     'license': 'OPL-1',
+    'price': 20.00,
+    'currency': 'USD',
 
     # Dependencias necesarias para heredar de account y sale
     'depends': ['base', 'account', 'sale', 'sale_management', 'sale_stock'],
